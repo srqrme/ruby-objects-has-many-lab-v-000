44a1750ec09afa80425e6303776e52f25bbea12a
Sharmaine Requerme <srqrme@gmail.com>
2018-08-08 20:16:54 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,18 @@
+class Song
+  attr_accessor :artist
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def name
+    @name
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+end
