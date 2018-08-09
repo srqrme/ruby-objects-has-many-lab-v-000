dd0a18a567d45b0b4a30c5729fbb4a4afae8a671
Sharmaine Requerme <srqrme@gmail.com>
2018-08-09 16:40:06 +0000

    Done.

--- lib/author.rb
+++ lib/author.rb
@@ -3,29 +3,29 @@ class Author
 
   @@post_count = 0
 
-  def initialize(title)
-    @title = title
-    @@posts = []
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def posts
+    @posts
   end
 
   def self.post_count
     @@post_count
   end
 
-  def add_post(post)
-    @@post << post
-    post.author = self
+  def add_post(title)
+    @posts << title
+    title.author = self
     @@post_count += 1
   end
 
   def add_post_by_title(title)
     post = Post.new(title)
-    @@posts << post
+    @posts << post
     post.author = self
     @@post_count += 1
-  end
-
-  def posts
-    @@posts
   end
 end
